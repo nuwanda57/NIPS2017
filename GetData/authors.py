9ea07b2f74ed5d3f32f1e02886b95ea2a3896b0e
Nuwanda <nuwanda57@yandex.ru>
2018-06-19 17:10:47 +0300

    Add files via upload

---
+++ GetData/authors.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+def get_authors():
+    with open("articles_refs.json") as i_file:
+        articles_refs = json.load(i_file)
+    for ref in articles_refs:
+        try:
+            f = open("Authors1/" + ref[29:-3] + "json")
+            f.close()
+        except:
+            ref1 = ref[:-4]
+            r = requests.get(ref1)
+            page = BeautifulSoup(r.text, "html.parser")
+            authors_page = page.find("ul", attrs={"class":"authors"})
+            authors = [i.text for i in authors_page.find_all("li")]
+            with open("Authors1/" + ref[29:-3] + "json", "w") as f_out:
+                json.dump(authors, f_out)
+
+
+def main():
+    get_authors()
+
+
+if __name__ == "__main__":
+    main()
