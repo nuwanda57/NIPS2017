42ff4609a52ba08c6e3602eef90ac044ecb3bff1
Nuwanda <nuwanda57@yandex.ru>
2018-06-20 14:43:32 +0300

    Update clean.py

--- ProcessData/TextProcessing/clean.py
+++ ProcessData/TextProcessing/clean.py
@@ -1,6 +1,5 @@
 import json
 import enchant
-from nltk.corpus import words as WORDS
 
 
 ENGLISH = enchant.Dict("en_US")
@@ -9,59 +8,6 @@ ENGLISH = enchant.Dict("en_US")
 def write_text_to_file(text, path):
     with open(path, 'w') as f_out:
         f_out.write(text)
-
-
-def count_numbers(p):
-    res = 0
-    for i in p:
-        if i.isdigit():
-            res += 1
-    return res
-
-
-def count_words(p):
-    words = p.split()
-    return len(words)
-
-
-def count_symbols(p):
-    res = 0
-    for i in p:
-        if i != " ":
-            res += 1
-    return res
-
-
-def average_word_length(p):
-    cnt = count_words(p)
-    if cnt == 0:
-        return 0
-    total = count_symbols(p)
-    return total / cnt
-
-
-def median_word_length(p):
-    words = p.split()
-    mas = []
-    for i in words:
-        mas.append(len(i))
-    return mas[len(mas) // 2]
-
-
-def count_letters(p):
-    res = 0
-    for i in p:
-        if i.isalpha():
-            res += 1
-    return res
-
-
-def count_special(p):
-    res = 0
-    for i in p:
-        if not i.isalpha() and i != " ":
-            res += 1
-    return res
 
 
 def save_only_words(text):
@@ -104,17 +50,6 @@ def save_only_words(text):
                     new_line += w
                     continue
             except:
-                continue
-            if count_special(w) / len(w) >= 0.5:
-                continue
-            if count_special(w) > 2:
-                continue
-            if len(w) <= 2:
-                continue
-            else:
-                # if new_line != "":
-                #     new_line += " "
-                # new_line += w
                 continue
         if new_line == "":
             lines.pop(i)
